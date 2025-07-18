b24dd7b45f309c628d3a2407371d315c3e78e0e8
AbelFeta <meetabelhere@gmail.com>
2025-07-18 15:03:16 +0300

    Js code that counts arguments passed to node.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,8 @@
-const arguments = process.argv.slice(2);
-if (arguments===0) {
+const args = process.argv.slice(2);
+if (args.length === 0) {
     console.log("No argument");
 }
-else if (arguments===1) {
+else if (args.length === 1) {
     console.log("Argument found");
 }
 else {
