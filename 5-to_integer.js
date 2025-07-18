99bde072f4b10d995d80e356a8aff4abda77618a
AbelFeta <meetabelhere@gmail.com>
2025-07-18 15:55:23 +0300

    JS code to convert an argument to integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 const number = Number(args[0]);
 if (!isNaN(number)) {
-    console.log(Math.floor(number));
+    console.log("My number: " + Math.floor(number));
 }
 else {
     console.log("Not a number");
